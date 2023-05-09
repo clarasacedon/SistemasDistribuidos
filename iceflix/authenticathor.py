9ddcc0ec37ed7cdcd3553a6a5773cc7769d89535
Clara Sacedón Ortega <clara.so1908@gmail.com>
2023-05-09 11:15:19 +0200

    Corrección errores

--- iceflix/authenticathor.py
+++ iceflix/authenticathor.py
@@ -28,28 +28,30 @@ class AuthenticatorData:
 
 class Authenticator:
     def refreshAuthorization(self, user, passwordHash):
-        password = self.usersDB.userPasswords.get(user)
+        password = self.currentUsers.userPasswords.get(user)
         if password is None:
             raise IceFlix.Unauthorized()
         elif password == passwordHash:
             token = secrets.token_hex(16)
             self.userUpdate.newToken(user,token,self.id)
+            clock = threading.Timer(120.0, self.revocations.revokeToken, args=[token, self.id])
+            clock.start()
         else:
             raise IceFlix.Unauthorized()
         return token
 
     def isAuthorized(self, userToken):
-        if userToken in self.usersDB.usersToken:
-            return True
+        if userToken in self.currentUsers.activeTokens:
+            auth = True
         else:
-            return False
+            auth = False
+        return auth
 
     def whois(self, userToken):
         if not self.isAuthorized(userToken):
             raise IceFlix.Unauthorized()
         else:
-            user = self.usersDB.usersToken.get(userToken)
-            return user
+            return self.currentUsers.activeTokens.get(userToken)
 
     def isAdmin(self, adminToken):
         return self.adminToken == adminToken
@@ -58,14 +60,14 @@ class Authenticator:
         if not self.isAdmin(adminToken):
             raise IceFlix.Unauthorized
 
-        self.users[user] = [{
+        self.currentUsers[user] = [{
             "token": secrets.token_hex(16),
             "passwordHash": passwordHash,
             "timestamp": time.mktime(datetime.datetime.now().timetuple())
         }]
 
         with open(PATH_USERS, 'w') as file:
-            json.dump(self.users, file)
+            json.dump(self.currentUsers, file)
 
         self.userUpdate.newUser(user, passwordHash, self.id)
 
@@ -73,18 +75,18 @@ class Authenticator:
         if not self.isAdmin(adminToken):
             raise IceFlix.Unauthorized
         
-        self.users.pop(user)
+        self.currentUsers.pop(user)
 
         with open(PATH_USERS, 'w') as file:
-            json.dump(self.users, file)
+            json.dump(self.currentUsers, file)
 
         self.userUpdate.removeUser(user, self.id)
 
     def bulkUpdate(self):
         auth_data = IceFlix.AuthenticatorData()
         auth_data.adminToken = self.adminToken
-        auth_data.currentUsers = {user: data[0]['passwordHash'] for user, data in self.users.items()}
-        auth_data.activeTokens = {user: data[0]['token'] for user, data in self.users.items() if data[0]['token']}
+        auth_data.currentUsers = {user: data[0]['passwordHash'] for user, data in self.currentUsers.items()}
+        auth_data.activeTokens = {user: data[0]['token'] for user, data in self.currentUsers.items() if data[0]['token']}
         
         return auth_data
 
@@ -96,16 +98,19 @@ class UserUpdate:
     def newToken(self, user, token, serviceId):
         if serviceId != self.authenticator.id and serviceId in self.authenticator.proxies:
             print('New token for ', user, ' received from', serviceId)
-            self.servant.users.usersToken[token] = user
+            self.servant.currentUsers.activeTokens[token] = user
         else:
             print('New token for ', user, ' from', serviceId, ' ignored')
 
     def revokeToken(self, token, serviceId):
         if serviceId != self.authenticator.id and serviceId in self.authenticator.proxies:
             print("Token ", token, " revoked from ", serviceId)
+            self.authenticator.currentUsers.activeTokens.pop(token)
         elif serviceId == self.authenticator.id:
             print('Token ', token, ' timed out')
-        self.authenticator.users.usersToken.pop(token)
+            self.authenticator.currentUsers.activeTokens.pop(token)
+        else:
+            print('Token ', token, ' from', serviceId, ' ignored')
 
     def newUser(self, user, passwordHash, serviceId):
         
